f8543ca10d9d0d2957ea626cde46846a8ee60b03
Константин Чирков <markus.rulit@gmail.com>
2025-05-17 15:07:29 +1000

    fixes

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция преобразования введенного числа в номер карты с маскировкой 6 цифр."""
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счёта и выдает его обратно с маской в виде **ХХХХ."""
+
+    return f"**{account_number[-4:]}"
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number(str(7000792289606361)))
+    print(get_mask_account(str(73654108430135874305)))
