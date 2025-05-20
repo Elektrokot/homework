c86ef1f7bc45d84fd1ad5c2555f1f08f020e1197
Константин Чирков <markus.rulit@gmail.com>
2025-05-21 09:44:38 +1000

    modified widget.py and __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+from src.widget import get_date, mask_account_card
+
+if __name__ == "__main__":
+    print(mask_account_card(str("Maestro 1596837868705199")))
+    print(mask_account_card(str("Счет 64686473678894779589")))
+    print(mask_account_card(str("MasterCard 7158300734726758")))
+    print(mask_account_card(str("Счет 35383033474447895560")))
+    print(mask_account_card(str("Visa Classic 6831982476737658")))
+    print(mask_account_card(str("Visa Platinum 8990922113665229")))
+    print(mask_account_card(str("Visa Gold 5999414228426353")))
+    print(mask_account_card(str("Счет 73654108430135874305")))
+    print(get_date(str("2024-03-11T02:26:18.671407")))
