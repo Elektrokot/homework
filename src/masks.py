a469664b01e0ebfc138a6b05ec049d58866f23a4
Константин Чирков <markus.rulit@gmail.com>
2025-05-20 23:45:15 +1000

    modified masks.py

--- src/masks.py
+++ src/masks.py
@@ -8,8 +8,3 @@ def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счёта и выдает его обратно с маской в виде **ХХХХ."""
 
     return f"**{account_number[-4:]}"
-
-
-if __name__ == "__main__":
-     print(get_mask_account(str(73654108430135874305)))
-     print(get_mask_card_number(str(7000792289606361)))
