8731fcf19acc0f4d2cf4a77378e74edd894b3a49
Константин Чирков <markus.rulit@gmail.com>
2025-06-09 14:04:55 +1000

    added htmlcov/ in gitignore, add conftest.py, add test_masks.py, add test_processing.py, add test_widget.py, changed masks.py, changed widget.py, changed src/__init__.py added lint pytest dev.

---
+++ tests/test_masks.py
@@ -0,0 +1,45 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+@pytest.mark.parametrize("string_card, expected_result_card", [
+    (1234567890123, "1234 56** **** 3"),
+    ("1234567890123455", "1234 56** **** 3455"),
+    (123456789012345, "1234 56** **** 345"),
+    ("1234-5678-9012-3459", "1234 56** **** 3459"),
+    (123456789012345678, "1234 56** **** 78"),
+    ("1234-5678 - 9012 - 3456", "1234 56** **** 3456"),
+    (1234567890123456789, "1234 56** **** 789"),
+    ("1234 5678 9012 3458", "1234 56** **** 3458"),
+    ("1234-5678 - 9012 - 3457", "1234 56** **** 3457")])
+
+def test_get_mask_card_number(string_card, expected_result_card):
+    assert get_mask_card_number(string_card) == expected_result_card
+
+
+@pytest.mark.parametrize("wrong_data_card", [
+    123456789012,
+    "12345678901234567890",
+    None,
+    ""])
+
+def test_get_mask_card_number_wrong(wrong_data_card):
+    with pytest.raises(ValueError):
+        get_mask_card_number(wrong_data_card)
+
+
+@ pytest.mark.parametrize("string_account, expected_result_account",
+        [(35383033474447895560, "**5560"),
+        ('73654108430135874305', "**4305")])
+
+def test_get_mask_account(string_account, expected_result_account):
+    assert get_mask_account(string_account) == expected_result_account
+
+
+@pytest.mark.parametrize("wrong_data_account",
+                         [7365410843013587, '', None])
+
+def test_get_mask_account_wrong(wrong_data_account):
+    with pytest.raises(ValueError):
+        get_mask_account(wrong_data_account)
---
+++ tests/test_widget.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.widget import get_date, mask_account_card
+
+
+def test_get_date(date_test):
+    assert get_date(date_test) == "11.03.2024"
+
+
+@pytest.mark.parametrize("string, expected_result",
+                         [("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                         ("Счет 64686473678894779589", "Счет **9589"),
+                         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229")])
+
+def test_mask_account_card(string, expected_result):
+    assert mask_account_card(string) == expected_result
